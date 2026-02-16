3d39c6cc6e6e3fb10c3a25af32afaa9369aa6caf
itsferlima <fer.nand.2149@gmail.com>
2026-02-16 18:14:34 +0000

    add clean code

---
+++ followers.ts
@@ -0,0 +1,67 @@
+const list = [
+    {
+        title: "User",
+        followers: 5
+    },
+    {
+        title: "Friendly",
+        followers: 50
+    },
+    {
+        title:"Famous",
+        followers: 500
+    },
+    {
+        title: "Super Star",
+        followers: 1000
+    }
+]
+
+export default async function getData(req, res){
+    const github = String(req.query.username)
+
+    if(!github){
+        return res.status(400).json({
+            message: `Please proive an user to search`
+        })
+    }
+
+    const response = await fetch(`https://api.github.com/users/${github}`)
+
+    if (response.status === 404){
+        return res.status(400).json({
+            message: `User with name "${github}" not found`
+        })
+    }
+
+
+const data = await Response.json()
+const orderList = list.sort((a,b) => b.followers - a.followers)
+const category = orderList.find( i=> data.followers > i.followers)
+
+const result = {
+    github,
+    category: category.title
+}
+
+return result
+
+}
+
+getData({ query:{
+    username: 'joseph'
+    
+} })
+
+const users= ['Fernanda', ' Gio', ' Love']
+
+const userStartWithLetterD = users.filter (user => {
+    return user.startsWith('F')
+})
+
+// Constante com nome claros, nao precisa ser pequeno
+// Nao usar DATA como constante, pq conforme vai crescendo o codigo, pode 
+function getUsers(){
+}
+
+//Nome Variaveis
---
+++ variaveis.ts
@@ -0,0 +1,10 @@
+const users= ['Fernanda', ' Gio', ' Love']
+
+const userStartWithLetterD = users.filter (user => {
+    return user.startsWith('F')
+})
+
+// Constante com nome claros, nao precisa ser pequeno
+// Nao usar DATA como constante, pq conforme vai crescendo o codigo, pode 
+function getUsers(){
+}
